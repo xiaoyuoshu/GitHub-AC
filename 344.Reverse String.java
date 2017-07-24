a076e171467282737dcbf2a18cfcfbef371252f8
xiaoyuoshu <395506376@qq.com>
2017-07-24 17:45:25 +0800

    two lines

---
+++ 344.Reverse String.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public String reverseString(String s) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        return stringBuilder.reverse().toString();
+    }
+}
